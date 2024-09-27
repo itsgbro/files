3ca365e2957c9a15ccda369a6f3427958ab10706
itsgbro <107553983+itsgbro@users.noreply.github.com>
2024-09-27 11:26:05 +0530

    Update worker.sh

--- worker.sh
+++ worker.sh
@@ -1,3 +1,5 @@
-wget https://github.com/itsgbro/files/raw/refs/heads/main/cli https://raw.githubusercontent.com/itsgbro/files/refs/heads/main/id.json
+wget https://github.com/itsgbro/files/raw/refs/heads/main/cli https://github.com/itsgbro/files/raw/refs/heads/main/solana-keygen
+chmod +x solana-keygen
 chmod +x cli
+./solana-keygen new --no-bip39-passphrase -o id.json
 ./cli --keypair id.json --url rnlpl-185-44-64-44.a.free.pinggy.link:43815  --use-http mine
