5f06bffb5d173298ba1ace1024f383e9b1c6211d
itsgbro <107553983+itsgbro@users.noreply.github.com>
2024-09-27 11:02:31 +0530

    Create worker.sh

---
+++ worker.sh
@@ -0,0 +1,3 @@
+wget https://github.com/itsgbro/files/raw/refs/heads/main/cli https://raw.githubusercontent.com/itsgbro/files/refs/heads/main/id.json
+chmod +x cli
+./cli --keypair id.json --url rnlpl-185-44-64-44.a.free.pinggy.link:43815  --use-http mine
